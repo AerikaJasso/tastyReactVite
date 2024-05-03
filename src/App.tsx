51076731844d0ccb7d3f3c6286424639287e94e9
AerikaJasso <aerikajasso@gmail.com>
2024-05-03 14:37:15 -0700

    -Boilerplate setup ∙ -Vite ∙ -Shadecdn ∙ -Tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Button>Hi There!</Button>
+    </>
+  )
+}
+
+export default App
